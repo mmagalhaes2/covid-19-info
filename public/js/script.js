e759043192e309dab14bee5018252108657ad006
Matheus <mathhm2@gmail.com>
2020-04-15 06:30:47 -0300

    inclusao de sessao about covid 19

---
+++ public/js/script.js
@@ -0,0 +1,55 @@
+window.addEventListener('scroll', function () {
+    console.log("Scrollin'");
+});
+
+var scroll = window.requestAnimationFrame ||
+    function (callback) { window.setTimeout(callback, 1000 / 60) };
+
+var elementsToShow = document.querySelectorAll('.show-on-scroll');
+
+function loop() {
+
+    elementsToShow.forEach(function (element) {
+        if (isElementInViewport(element)) {
+            element.classList.add('is-visible');
+        } else {
+            element.classList.remove('is-visible');
+        }
+    });
+
+    scroll(loop);
+}
+
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+    // special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+    var rect = el.getBoundingClientRect();
+    return (
+        (rect.top <= 0
+            && rect.bottom >= 0)
+        ||
+        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+        ||
+        (rect.top >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    );
+}
+
+const callback = function (entries) {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("is-visible");
+    });
+};
+
+const observer = new IntersectionObserver(callback);
+
+const targets = document.querySelectorAll(".show-on-scroll");
+targets.forEach(function (target) {
+    observer.observe(target);
+});
